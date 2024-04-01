1515a2cfa93597f3316f921c13561bd066161c96
woyxiang <119841810+woyxiang@users.noreply.github.com>
2024-04-01 22:50:23 +0800

    Initial commit

---
+++ src/main/java/name/xrcn/XRCNClient.java
@@ -0,0 +1,10 @@
+package name.xrcn;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class XRCNClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
